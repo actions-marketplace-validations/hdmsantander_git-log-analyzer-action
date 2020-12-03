9b33810be7b31ecefb418f0687f503c80e8d674e
Roberto Fabián Escobar Santander <54561532+hdmsantander@users.noreply.github.com>
2020-12-02 21:14:26 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 HOME=/home/gitloganalyzer
 
-if [ -n "$INPUT_INIT_DATE"] && [ -n "$INPUT_END_DATE"]; then
+if [ -n "$INPUT_INIT_DATE" ] && [ -n "$INPUT_END_DATE" ]; then
     git log --pretty=format:'[%h] %an %ad %s' --date=short --numstat --after=$INPUT_INIT_DATE --before=$INPUT_END_DATE > $HOME/git.log
 else
     MIN_DATE=$(date +'%Y-%m-%d' -d 'last month')
@@ -15,13 +15,13 @@ FREQUENCIES=`cat $HOME/frecuencies.csv`
 java -jar $HOME/gitloganalyzer.jar -f $HOME/git.log -coupling $INPUT_MIN_COCHANGES > $HOME/coupling.csv
 COUPLING=`cat $HOME/coupling.csv`
 
-if [ -n "$FREQUENCIES"]; then
+if [ -n "$FREQUENCIES" ]; then
     echo "::set-output name=frecuencies::$FREQUENCIES"
 else
     echo "::set-output name=frecuencies::empty"
 fi
 
-if [ -n "$COUPLING"]; then
+if [ -n "$COUPLING" ]; then
     echo "::set-output name=coupling::$COUPLING"
 else
     echo "::set-output name=coupling::empty"
